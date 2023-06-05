07e412a05f6c7726fba5204f4207df67ff807ff9
w3villa-jagendra <you@example.com>
2023-06-05 14:59:03 +0530

    updated the login sign up modal

--- script/script.js
+++ script/script.js
@@ -1,30 +1,28 @@
-// ******************
-// Why-buy-section 
-// ******************
 
 
-fetch("script/why_buy.json")
-  .then(response => response.json())
-  .then(data => {
-    const carousel = document.getElementById("y-buy");
-    for (let i = 0; i < data.why_buy.length; i++) {
-      let div = document.createElement("div");
-      div.classList.add("item", "card", "why-card");
-      let img = document.createElement("img");
-      img.classList.add("card-img-top");
-      img.src = data.why_buy[i].image;
-      img.alt = data.why_buy[i].name;
-      let button = document.createElement("button");
-      button.classList.add("btn", "btn-light", "btn-why");
-      button.textContent = data.why_buy[i].name;
-      div.appendChild(img);
-      div.appendChild(button);
-      if (carousel) {
-        carousel.appendChild(div);
-      }
-    }
 
-  });
+// fetch("script/why_buy.json")
+//   .then(response => response.json())
+//   .then(data => {
+//     const carousel = document.getElementById("y-buy");
+//     for (let i = 0; i < data.why_buy.length; i++) {
+//       let div = document.createElement("div");
+//       div.classList.add("item", "card", "why-card");
+//       let img = document.createElement("img");
+//       img.classList.add("card-img-top");
+//       img.src = data.why_buy[i].image;
+//       img.alt = data.why_buy[i].name;
+//       let button = document.createElement("button");
+//       button.classList.add("btn", "btn-light", "btn-why");
+//       button.textContent = data.why_buy[i].name;
+//       div.appendChild(img);
+//       div.appendChild(button);
+//       if (carousel) {
+//         carousel.appendChild(div);
+//       }
+//     }
+
+//   });
 
 
 
@@ -35,11 +33,7 @@ document.getElementById("openModalBtn").addEventListener("click", function() {
     document.getElementById("myLoginModal").style.display = "block";
   });
   
-  // Close modal when close button is clicked
-  document.getElementsByClassName("close")[0].addEventListener("click", function() {
-    document.getElementById("myLoginModal").style.display = "none";
-  });
-  
+
   document.getElementsByClassName("cancel-btn")[0].addEventListener('click', function(){
     document.getElementById("myLoginModal").style.display ="none";
   })
