2768e4642181618835fc1904b8ac1f80216bd0b2
jagemdra <jagendrasrivastava007@gmail.com>
2023-08-30 12:01:17 +0530

    improved owl carsoule of the why buy section

--- script/fpOwl.js
+++ script/fpOwl.js
@@ -1,129 +1,203 @@
 
 
 
-  fetch("script/fp_items.json")
-    .then(response => response.json())
-    .then(data => {
-      const carousel = document.getElementById("fpro-items");
-      for (let i = 0; i < data.fp_items.length; i++) {
-        let div = document.createElement("div");
-        div.classList.add("item", "card");
-        let img = document.createElement("img");
-        img.classList.add("card-img-top");
-        img.src = data.fp_items[i].fp_img;
-        img.alt = data.fp_items[i].fp_name;
-        let section = document.createElement("section");
+  // fetch("script/fp_items.json")
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     const carousel = document.getElementById("fpro-items");
+  //     for (let i = 0; i < data.fp_items.length; i++) {
+  //       let div = document.createElement("div");
+      
+  //       div.classList.add("item");
+  //      let div2 =  div.appendChild(div);
+  //         div2.classList.add("card");
+  //         console.log(div2);
+  //       let img = document.createElement("img");
+  //       img.classList.add("card-img-top");
+  //       img.src = data.fp_items[i].fp_img;
+  //       img.alt = data.fp_items[i].fp_name;
+  //       let section = document.createElement("section");
 
-        section.innerHTML = ` <div class="card-header">
-           <a href="product.html?id=${data.fp_items[i].fp_id}"> ${data.fp_items[i].fp_name}</a>
-            <a href="product.html?id=${data.fp_items[i].fp_id}"> ${data.fp_items[i].fp_model}</a>
-        </div>
-        <div class="card-body">
-            <h6>${data.fp_items[i].fp_type}</h6>
-            <p class="price">${data.fp_items[i].fp_newPrice} <del style="color:grey">${data.fp_items[i].fp_oldPrice}</del></p>
-            <div class="wishlist">
-                <input type="number" id="quantity" name="quantity" placeholder="1" min="1" max="5">
-                <button type="button" class="btn btn-primary btn-sm" onclick=  "AddWish('${data.fp_items[i].fp_id}' , 'addtocart')"  >Add to cart</button>
-                <i class="fa-regular fa-heart icon" id="heart${data.fp_items[i].fp_id}"  onclick= "AddWish('${data.fp_items[i].fp_id}' , 'wishlist')" ></i>
-                <i class="fa-solid fa-arrow-right-arrow-left icon"></i>
-            </div>
-        </div>
-        <div class="card-foot">
-            <a href="#"><i class="fa-regular fa-circle-question"></i> Questions</a>
-            <a href="#"> <i class="fa-solid fa-dollar-sign"></i> Buy Now</a>
-        </div>`;
+  //       section.innerHTML = ` <div class="card-header">
+  //          <a href="product.html?id=${data.fp_items[i].fp_id}"> ${data.fp_items[i].fp_name}</a>
+  //           <a href="product.html?id=${data.fp_items[i].fp_id}"> ${data.fp_items[i].fp_model}</a>
+  //       </div>
+  //       <div class="card-body">
+  //           <h6>${data.fp_items[i].fp_type}</h6>
+  //           <p class="price">${data.fp_items[i].fp_newPrice} <del style="color:grey">${data.fp_items[i].fp_oldPrice}</del></p>
+  //           <div class="wishlist">
+  //               <input type="number" id="quantity" name="quantity" placeholder="1" min="1" max="5">
+  //               <button type="button" class="btn btn-primary btn-sm" onclick=  "AddWish('${data.fp_items[i].fp_id}' , 'addtocart')"  >Add to cart</button>
+  //               <i class="fa-regular fa-heart icon" id="heart${data.fp_items[i].fp_id}"  onclick= "AddWish('${data.fp_items[i].fp_id}' , 'wishlist')" ></i>
+  //               <i class="fa-solid fa-arrow-right-arrow-left icon"></i>
+  //           </div>
+  //       </div>
+  //       <div class="card-foot">
+  //           <a href="#"><i class="fa-regular fa-circle-question"></i> Questions</a>
+  //           <a href="#"> <i class="fa-solid fa-dollar-sign"></i> Buy Now</a>
+  //       </div>`;
 
-        div.appendChild(img);
-        div.appendChild(section);
-        if (carousel) {
-          carousel.appendChild(div);
-        }
-      }
-    });
+  //       div2.appendChild(img);
+  //       div2.appendChild(section);
+  //       if (carousel) {
+  //         carousel.appendChild(div);
+  //       }
+  //     }
+  //   });
 
 
 
-    // Change the heart-icon  color 
+  //   // Change the heart-icon  color 
 
-  function toggleHeartColor(icon) {
-    // Toggle the class name to change the color
-    icon.classList.toggle("heart-red");
-  }
+  // function toggleHeartColor(icon) {
+  //   // Toggle the class name to change the color
+  //   icon.classList.toggle("heart-red");
+  // }
 
 
   
 
 
 
-  function AddWish(id , action)
-  {  
+  // function AddWish(id , action)
+  // {  
 
 
-    if(action === "addtocart")
-    {
+  //   if(action === "addtocart")
+  //   {
 
-      fetch("script/fp_items.json")
-      .then(response => response.json())
-      .then(data => {
+  //     fetch("script/fp_items.json")
+  //     .then(response => response.json())
+  //     .then(data => {
           
-        for(let i =0;i<data.fp_items.length;i++)
-        {
-          if(id === data.fp_items[i].fp_id)
-          {
+  //       for(let i =0;i<data.fp_items.length;i++)
+  //       {
+  //         if(id === data.fp_items[i].fp_id)
+  //         {
     
           
-           const item =  document.getElementById("addtocart")
+  //          const item =  document.getElementById("addtocart")
     
-           item.innerHTML += `
-           <div class="wishlist-item d-flex ">
-           <img src="${data.fp_items[i].fp_img}" alt="" class="wish-img">
+  //          item.innerHTML += `
+  //          <div class="wishlist-item d-flex ">
+  //          <img src="${data.fp_items[i].fp_img}" alt="" class="wish-img">
     
-            <p class="wish-quantity"> ${data.fp_items[i].fp_name}</p>
+  //           <p class="wish-quantity"> ${data.fp_items[i].fp_name}</p>
            
-          </div> 
-            `
+  //         </div> 
+  //           `
             
             
-          }
-        }
+  //         }
+  //       }
     
-      });
-    }
+  //     });
+  //   }
 
     
-   else if(action === "wishlist")
-   {
+  //  else if(action === "wishlist")
+  //  {
+  // fetch("script/fp_items.json")
+  // .then(response => response.json())
+  // .then(data => {
+      
+  //   for(let i =0;i<data.fp_items.length;i++)
+  //   {
+  //     if(id === data.fp_items[i].fp_id)
+  //     {
+
+      
+  //      const item =  document.getElementById("wishlist-item ")
+
+  //      item.innerHTML += `
+  //      <div class="wishlist-item d-flex ">
+  //      <img src="${data.fp_items[i].fp_img}" alt="" class="wish-img">
+
+  //       <p class="wish-quantity"> ${data.fp_items[i].fp_name}</p>
+       
+  //     </div> 
+  //       `
+        
+  //       let heartColor = `heart${data.fp_items[i].fp_id}`;
+        
+  //      let h=  document.getElementById(heartColor);
+  //      h.classList.add("heart-red");
+        
+  //     }
+  //   }
+
+  // });
+  // }
+  // }
+
+  
+
+
+
+
+
+
   fetch("script/fp_items.json")
   .then(response => response.json())
   .then(data => {
+ 
+
+ let container = document.createElement("div");
+     container.classList.add("container");
+
+     
+     let oWrap = document.createElement("div");
+
+     oWrap.classList.add("carousel-wrap");
+
+     container.appendChild(oWrap);
+
+
+     let cWrap = document.createElement("div");
+     cWrap.classList.add("owl-carousel owl-theme");
+
+   let wrap =   oWrap.appendChild(cWrap);
+
+
+     const carousel = document.getElementById("fpro-items");
+
+wrap.appendChild("carousel");
+    for (let i = 0; i < data.fp_items.length; i++) {
+      let div = document.createElement("div");
+      div.classList.add("item");
       
-    for(let i =0;i<data.fp_items.length;i++)
-    {
-      if(id === data.fp_items[i].fp_id)
-      {
-
+      // Create a nested div
+      let div2 = document.createElement("div");
+      div2.classList.add("card");
       
-       const item =  document.getElementById("wishlist-item ")
+      // Append the nested div to the main div
+      div.appendChild(div2);
+      
+      let img = document.createElement("img");
+      img.classList.add("card-img-top");
+      img.src = data.fp_items[i].fp_img;
+      img.alt = data.fp_items[i].fp_name;
 
-       item.innerHTML += `
-       <div class="wishlist-item d-flex ">
-       <img src="${data.fp_items[i].fp_img}" alt="" class="wish-img">
-
-        <p class="wish-quantity"> ${data.fp_items[i].fp_name}</p>
-       
-      </div> 
-        `
-        
-        let heartColor = `heart${data.fp_items[i].fp_id}`;
-        
-       let h=  document.getElementById(heartColor);
-       h.classList.add("heart-red");
-        
-      }
+      console.log( img.src);
+      
+      div2.appendChild(img); // Append the img to the nested div
+      
+      carousel.appendChild(div); // Append the main div to the carousel
     }
+  })
+  .catch(error => console.error("Error fetching data:", error));
 
-  });
-  }
-  }
 
-  
+  // <div class="container">
+  //          <div class="carousel-wrap"> 
+  //               <div class="owl-carousel owl-theme"  id="fpro-items">
+
+                 
+                  
+  //                  here all the content of the cards will be added 
+              
+              
+
+  //               </div>
+  //       </div> 
+  //       </div>
--- script/script.js
+++ script/script.js
@@ -5,6 +5,89 @@
 
 
 
+function toggleTopCategories(elem) {
+    let prevElem = document.getElementsByClassName(
+      "active_btn_toogle_categories"
+    )[0];
+    prevElem.classList.remove("active_btn_toogle_categories");
+    elem.classList.add("active_btn_toogle_categories");
+  }
+  // function toogle_featureproduct(elemtop) {
+  //   let previouselement = document.getElementsByClassName(
+  //     "active_btn_toogle_top_categories"
+  //   )[0];
+  //   previouselement.classList.remove("active_btn_toogle_top_categories");
+  //   elemtop.classList.add("active_btn_toogle_top_categories");
+  // }
+  // function toogle_ourblog(blogelement) {
+  //   let previousblogele = document.getElementsByClassName(
+  //     "active_btn_toogle_blog"
+  //   )[0];
+  //   previousblogele.classList.remove("active_btn_toogle_blog");
+  //   blogelement.classList.add("active_btn_toogle_blog");
+  // }
+
+
+
+// let a = document.getElementsByClassName("whyus_crowsel")[0];
+let b = document.querySelector(".whyus_crowsel");
+// let featureproductcat = document.querySelector(".featureproductcat-container");
+// let newfashioncrowesl = document.querySelector(".newfashioncrowesl");
+// let blogcrowsel = document.querySelector(".blog-crowsel");
+// let sayingaboutcarousel = document.querySelector(".saying-about-carousel");
+// let mostviedmainsection = document.querySelector(".mostvied-main-section");
+async function dataadd(category) {
+  let response = await fetch("script/product.json");
+  let data = await response.json();
+  let content = data[category];
+  ihtml = `<div class="owl-carousel owl-theme" id="whyus">`;
+  for (i in content) {
+    ihtml += `
+    <div class="item1 img-topcategories">
+    <img src="${content[i].img}" alt="" />
+    <a href= "search.html">${content[i].name}</a>
+ 
+    </div>
+    `;
+  }
+  ihtml += `</div>`;
+  b.innerHTML = ihtml;
+  whyusCarousel();
+}
+
+dataadd((category = "topCategory"));
+
+
+
+function whyusCarousel() {
+  $("#whyus").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      426: {
+        items: 2,
+      },
+      700: {
+        items: 3,
+      },
+      900: {
+        items: 4,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+}
+
+
+
+
 
 
 
