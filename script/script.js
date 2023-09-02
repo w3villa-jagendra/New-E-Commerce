676ef7104e44762203ef4736bd4139e5294ffc39
jagemdra <jagendrasrivastava007@gmail.com>
2023-09-02 21:44:17 +0530

    fixed featured product section

--- script/script.js
+++ script/script.js
@@ -55,7 +55,7 @@ async function dataadd(category) {
   whyusCarousel();
 }
 
-dataadd((category = "topCategory"));
+dataadd( "topCategory");
 
 
 
@@ -90,6 +90,117 @@ function whyusCarousel() {
 
 
 
+// featured Product
+
+function toggleFeaturedCategory(elemtop) {
+  let previouselement = document.getElementsByClassName(
+    "active_btn_toogle_top_categories"
+  )[0];
+  previouselement.classList.remove("active_btn_toogle_top_categories");
+  elemtop.classList.add("active_btn_toogle_top_categories");
+}
+// function toogle_ourblog(blogelement) {
+//   let previousblogele = document.getElementsByClassName(
+//     "active_btn_toogle_blog"
+//   )[0];
+//   previousblogele.classList.remove("active_btn_toogle_blog");
+//   blogelement.classList.add("active_btn_toogle_blog");
+// }
+
+
+
+// let a = document.getElementsByClassName("whyus_crowsel")[0];
+
+
+let featureproductcat = document.querySelector(".featuredCar");
+// let newfashioncrowesl = document.querySelector(".newfashioncrowesl");
+// let blogcrowsel = document.querySelector(".blog-crowsel");
+// let sayingaboutcarousel = document.querySelector(".saying-about-carousel");
+// let mostviedmainsection = document.querySelector(".mostvied-main-section");
+async function featuredDataAdd(category) {
+let response = await fetch("script/featured.json");
+let data = await response.json();
+let content = data[category];
+ihtml = `<div class="owl-carousel owl-theme" id="featuredCat">`;
+for (i in content) {
+  ihtml += `
+  
+  <div class="card item1 img-topcategories">
+  <div class="comman-lable ">
+      <span class=" lable-1"> Hot</span>
+      <span class="lable-2">-70%</span>
+  </div>
+
+  <div class="rotated-lable">
+      <span class="lable-4">CUSTOM LABLE</span>
+  </div>
+
+  <img src="${content[i].fp_img}" class="card-img-top" alt="...">
+  <div class="card-header">
+      <a href="#">${content[i].fp_name}</a>
+      <a href="#">${content[i].fp_model} </a>
+
+  </div>
+  <div class="card-body">
+      <h6>${content[i].fp_type}</h6>
+
+      <p class="price">${content[i].fp_newPrice} <del style="color:grey">${content[i].fp_oldPrice}</del></p>
+      <div class="wishlist">
+          <input type="number" id="quantity" name="quantity" placeholder="1" min="1" max="5">
+          <button type="button" class="btn btn-primary btn-sm">Add to cart</button>
+          <i class="fa-regular fa-heart icon"></i>
+          <i class="fa-solid fa-arrow-right-arrow-left icon"></i>
+      </div>
+  </div>
+  <div class="card-foot">
+
+      <a href="#"><i class="fa-regular fa-circle-question"></i> Questions</a>
+      <a href="#"> <i class="fa-solid fa-dollar-sign"></i> Buy Now</a>
+
+
+
+
+  </div>
+
+
+</div>
+
+  `;
+}
+ihtml += `</div>`;
+featureproductcat.innerHTML = ihtml;
+featuredCarousel();
+}
+
+featuredDataAdd((category = "featured"));
+
+
+
+function featuredCarousel() {
+$("#featuredCat").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: true,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    426: {
+      items: 2,
+    },
+    700: {
+      items: 3,
+    },
+    900: {
+      items: 4,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+}
 
 
 
